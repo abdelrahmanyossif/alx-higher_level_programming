51df18a0e3a73bdcd0d8a03c69dcdb6717965f46
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-11-04 07:09:31 -0800

    k

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-N queens backtracking program to print the coordinates of N queens
-on an NxN grid such that they are all in non-attacking positions
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
 """
 
 
